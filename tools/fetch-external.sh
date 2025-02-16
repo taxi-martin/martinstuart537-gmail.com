8252c22dbb5c075e770b49e8062df9c4e017bdfd
taxi-martin <121961921+taxi-martin@users.noreply.github.com>
2025-02-16 13:15:56 +0000

    Initialize nazrhom-patch-1

---
+++ tools/fetch-external.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+# This script pulls in external documentation that should be edited in the corresponding upstream repo
+
+# Use node-jq if jq is not pre-installed in the environment nor set in path
+which jq && JQ="$(which jq)" || JQ="../../node_modules/node-jq/bin/jq"
+
+# get latest CLI docs
+cd pages/reference/ && {
+  curl -O -L https://github.com/balena-io/balena-cli/raw/master/doc/cli.markdown
+  mv cli.markdown balena-cli.md
+  cd -
+} &
+
+# get SDK README
+cd shared/sdk/ && {
+  curl -O -L https://raw.githubusercontent.com/balena-io/balena-sdk/master/README.md
+  # Extract deprecation text
+  ../../tools/extract-markdown.sh "Deprecation policy" <README.md >deprecation-policy.md
+  cd -
+} &
+
+# get latest supervisor API docs
+cd pages/reference/supervisor/ && {
+  curl -O -L https://github.com/balena-io/balena-supervisor/raw/master/docs/API.md
+  mv API.md supervisor-api.md
+  cd -
+} &
+
+# get latest diagnostics docs
+cd pages/reference/ && {
+  curl -O -L https://github.com/balena-io/device-diagnostics/raw/master/diagnostics.md
+  cd -
+} &
+
+# get latest supervisor update-lock docs
+cd pages/learn/deploy/release-strategy/ && {
+  curl -O -L https://github.com/balena-io/balena-supervisor/raw/master/docs/update-locking.md
+  cd -
+} &
+
+# get meta-balena README and extract partials
+cd shared/meta-balena/ && {
+  curl -O -L https://raw.githubusercontent.com/balena-os/meta-balena/master/README.md
+  mv README.md meta-balena.md
+  # Extract modem text
+  ../../tools/extract-markdown.sh "Modems" <meta-balena.md >supported-modems.md
+  # Extract Wifi adapters
+  ../../tools/extract-markdown.sh "WiFi Adapters" <meta-balena.md >supported-wifi-adapters.md
+  # Extract config.json
+  ../../tools/extract-markdown.sh "config.json" <meta-balena.md >config-json.md
+  cd -
+} &
+
+# get integrations
+cd pages/learn/develop/integrations/ && {
+  curl -O -L https://raw.githubusercontent.com/balenalabs/google-iot/master/README.md
+  mv README.md google-iot.md
+  curl -O -L https://raw.githubusercontent.com/balena-io-playground/balena-azure-iot-hub/master/README.md
+  mv README.md azure-iot-hub.md
+  cd -
+} &
+
+# Masterclasses
+cd pages/learn/more/masterclasses/ && {
+  curl -O -L https://raw.githubusercontent.com/balena-io/balena-cli-masterclass/master/README.md
+  mv README.md cli-masterclass.md
+  curl -O -L https://raw.githubusercontent.com/balena-io/balena-cli-advanced-masterclass/master/README.md
+  mv README.md advanced-cli.md
+  curl -O -L https://raw.githubusercontent.com/balena-io/balenaos-masterclass/master/README.md
+  mv README.md host-os-masterclass.md
+  curl -O -L https://raw.githubusercontent.com/balena-io/services-masterclass/master/README.md
+  mv README.md services-masterclass.md
+  curl -O -L https://raw.githubusercontent.com/balena-io-projects/balena-fleet-management-masterclass/master/README.md
+  mv README.md fleet-management.md
+  curl -O -L https://raw.githubusercontent.com/balena-io/debugging-masterclass/master/README.md
+  mv README.md device-debugging.md
+  curl -O -L https://raw.githubusercontent.com/balena-io/docker-masterclass/master/README.md
+  mv README.md docker-masterclass.md
+  cd -
+} &
+
+# pull in balenaLabs GitHub projects
+cd shared/projects/ && {
+  echo "Name|Description
+---|---" | tee balena-labs-projects.md balena-example-projects.md balenablocks.md >/dev/null
+  curl https://api.github.com/orgs/balenalabs/repos?per_page=30 | $JQ -r 'sort_by(-.stargazers_count) |  (.[] | [.name,.html_url,.description] | "[\(.[0])](\(.[1]))|\(.[2] // "")") ' >>balena-labs-projects.md
+  curl https://api.github.com/orgs/balena-io-examples/repos?per_page=100 | $JQ -r 'sort_by(-.stargazers_count) |  (.[] | [.name,.html_url,.description] | "[\(.[0])](\(.[1]))|\(.[2] // "")") ' >>balena-example-projects.md
+  curl https://api.github.com/orgs/balenablocks/repos?per_page=30 | $JQ -r 'sort_by(-.stargazers_count) |  (.[] | [.name,.html_url,.description] | "[\(.[0])](\(.[1]))|\(.[2] // "")") ' >>balenablocks.md
+  cd -
+} &
+
+# get latest node SDK docs
+cd pages/reference/sdk/ && {
+  curl -O -L https://github.com/balena-io/balena-sdk/raw/master/DOCUMENTATION.md
+  echo "# Balena Node.js SDK" >node-sdk.md
+  tail -n +2 DOCUMENTATION.md >>node-sdk.md
+  cd -
+} 
+
+# get latest python SDK docs
+cd pages/reference/sdk/ && {
+  curl -O -L https://github.com/balena-io/balena-sdk-python/raw/master/DOCUMENTATION.md
+  mv DOCUMENTATION.md python-sdk.md
+  cd -
+} 
+
+wait
